b597f18adbe5f415885d0e719ad129f9c51b90e8
gbukovszki <46926258+gbukovszki@users.noreply.github.com>
2019-02-18 13:35:54 +0100

    Update Plugin.php

--- Plugin.php
+++ Plugin.php
@@ -3,12 +3,12 @@
 namespace Kanboard\Plugin\AutomaticAction;
 
 use Kanboard\Core\Plugin\Base;
-use Kanboard\Plugin\AutomaticAction\Action\TaskRename;
+use Kanboard\Plugin\AutomaticAction\Action\PrioByValue;
 
 class Plugin extends Base
 {
     public function initialize()
     {
-        $this->actionManager->register(new TaskRename($this->container));
+        $this->actionManager->register(new PrioByValue($this->container));
     }
 }
